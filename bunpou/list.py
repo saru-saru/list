f8a4b97a44eccfa32cde13f935c7c2d2d842b296
akito <akito@localhost.localdomain>
2017-08-30 14:33:58 +0900

    lists

---
+++ bunpou/list.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+
+list = [
+[
+ ["""He ( ) his job last month.
+ 1:has quit 2:quit 3:has been quitting 4:was quite""","2",
+ "彼は先月仕事をやめた"],
+ 
+ ["""Ken is a last-minute person and often ( ) late for meetings.
+ 1:arrive 2:arrives 3:will arrive 4:arrived.""","2",
+ "ケンはギリギリにならないとやらない人で、よく会議に遅れてくる。"],
+ 
+ ["""Someting strange ( ) at school yesterday when we were having lunch.
+ 1:happend 2:happens 3:is happend 4:was happend""","1",
+ "昨日、私たちが昼食を食べているときに学校で何か奇妙なことがあった"],
+ 
+ ["""When Nancy called me last night,I ( ) a video game.
+ 1:am playing 2:play 3:have played 4:was playing""","4",
+ "昨夜ナンシーが私に電話してきたとき、私はテレビゲームをしていた"],
+ 
+ ["""Mr.Johnson ( ) to Chicago on business next month.
+ 1:go 2:is going 3:gone 4:went""","2",
+ "ジョンソン氏は来月仕事でシカゴへ行く予定だ"],
+ 
+ ["""Do you think they ( ) me nicely if I go there again?
+ 1:will treat 2:had treated 3:treat 4:treated""","1",
+ "再びそこにいったら、彼らは私によくしてくれると思いますか。"],
+ 
+ ["""Sally and her friend are ( ) to go to Spain.
+ 1:enjoy 2:going 3:will 4:goes""","2",
+ "サリーと彼女の友達はスペインへ行く予定だ"],
+ 
+ ["""Mr.Richard  ( ) his position as an ALT at the beginning of the next month.
+ 1:has been started 2:has started 3:started 4:will be starting""","4",
+ "リチャード先生は、来月の初めにALTとして勤め始めることになっている"], 
+ 
+ ["""A:Did you manage to answer all the questions on the test?
+ B:I was ( ) to finish the last question when the bell rang.
+ 1:about 2:close 3:over 4:out""","1",
+ "A:テストの問題に全部答えられた？  B:最後の問題をもうすぐ書き終わるってところでチャイムがなったんだよ"],
+ 
+ ["""They are good friends and ( ) each other since they were children.
+ 1:had known 1:have known 2:knew 3:are knowing""","2",
+ "彼らは親友で子どものころから知り合いだ"],
+ 
+ ["""A:Are Mary and Tom still living in Tokyo?
+ B:No. They ( ) to Beijing.
+ 1:are just moved 2:had just moved 3:have just moved 4:will just moved""","3",
+ "A:メアリーとトムはまだ東京に住んでるんですか？  B:いいえ。彼らはちょうど北京へ引っ越したところです"],
+ 
+ ["""I\'ve never been ( ) China, but I\'d like to go.
+ 1:in 2:at 3:to 4:on""","3",
+ "私は中国に一度も行ったことはないが、行きたいと思っている。"],
+ 
+ ["How (absent/been/from/has/he/long/school)?",
+ "How long has he been absent from school?",
+ "彼は学校をどれくらい欠席していますか?"],
+ 
+ ["""Ken and Mari ( ) at their grandmother's House in Hawaii since last weekend.
+ They will go back to Japan tomorrow.
+ 1:will be staying 2:stay 3:have been staying 4:stayed""","3",
+ "ケンとマリは先週末からハワイの祖母の家に泊まっている。彼らは明日、日本に戻るだろう"],
+ 
+ ["""A:Have you ever been to London?
+ B:Yes,actually,I ( ) there last year.
+ 1:go 2:have gone 3:went 4:would go""","3",
+ "A:ロンドンに行ったことありますか? B:ええ、実は去年行ったんです。"],
+ 
+ ["""When Dave reached the station,he found that his (  ) left.
+ 1:did already 2:has already 3:had alredy 4:was already""","3",
+ "デイヴが駅についたとき、彼の乗る電車がすでに発車していたことに気づいた。"],
+ 
+ ["""I lost the map that I ( ) the day before.
+ 1:buy 2:was buying 3:have bought 4:had bought""","4",
+ "私は前日に買った地図をなくした"],
+ 
+ ["""My sister seemed very sleepy because she ( ) all night.
+ 1:sudies 2:is studying 3:has been studying 4:had been studying""","4",
+ "姉(妹)は一晩中勉強していたので、とても眠そうに見えた。"],
+ 
+ ["""In 47 B.C., Julius Caesar said that ( ) won a great victory.
+ 1:he does 2:had he 3:he had 4:does he""","3",
+ "紀元前47年、ジュリアス・シーザーは大勝利を収めたといった。"],
+ 
+ ["""The theacher taught us that Queen Victoria ( ) in 1901.
+ 1:died 2:dies 3:dead  4:has been dead""","1",
+ "先生は私たちに、ヴィクトリア女王は1901年になくなったと教えた"],
+ 
+ 
+ 
+
